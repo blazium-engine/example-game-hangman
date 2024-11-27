1e2e74c714fedc9a21d9f1d4d6208cc885b02b24
Dragos Daian <daian.dragos@yahoo.com>
2024-11-27 18:09:59 +0100

    add lobby stuff

--- menus/lobby_browser.gd
+++ menus/lobby_browser.gd
@@ -1,11 +1,6 @@
-extends Control
+extends ColorRect
 
 var main_menu_scene : PackedScene = load("res://menus/main_menu.tscn")
-@onready var 
 
 func _on_button_main_menu_pressed() -> void:
 	get_tree().change_scene_to_packed(main_menu_scene)
-
-
-func _on_check_box_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,34 +1,25 @@
 extends Control
 
-var lobby_browser_scene : PackedScene = load("res://menus/lobby_browser/lobby_browser.tscn")
+var lobby_browser_scene : PackedScene = load("res://menus/lobby_browser.tscn")
 var lobby_creator_scene : PackedScene = load("res://menus/lobby_creator.tscn")
-var reconnects = 0
-@onready var peer_name : LineEdit = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/LineEdit
 @onready var lobby_connection_label : Label = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/Label
-
 func _ready() -> void:
-	peer_name.text = GlobalLobbyClient.peer.peer_name
 	GlobalLobbyClient.append_log.connect(_on_logs)
 	GlobalLobbyClient.disconnected_from_lobby.connect(_on_disconnect)
 	connect_to_lobby()
 
 func connect_to_lobby():
-	#GlobalLobbyClient.server_url = "ws://localhost:8080/connect"
 	var connected = GlobalLobbyClient.connect_to_lobby("Hangman")
 	if connected:
 		lobby_connection_label.text = "Lobby Service: Connected"
+	else:
+		lobby_connection_label.text = "Lobby Service: Disconnected"
 
 func _on_logs(command: String, logs:String):
 	print(command, " ", logs)
 	
 func _on_disconnect():
-	lobby_connection_label.text = "Lobby Service: Retrying"
-	if reconnects > 5:
-		push_error("Cannot connect")
-		return
-	reconnects += 1
-	await get_tree().create_timer(0.5 * reconnects).timeout
-	connect_to_lobby()
+	print("Disconnected")
 
 func _on_button_join_public_pressed() -> void:
 	get_tree().change_scene_to_packed(lobby_browser_scene)
@@ -36,9 +27,3 @@ func _on_button_join_public_pressed() -> void:
 
 func _on_button_lobby_pressed() -> void:
 	get_tree().change_scene_to_packed(lobby_creator_scene)
-
-
-func _on_line_edit_text_submitted(new_text: String) -> void:
-	var result :LobbyResult= await GlobalLobbyClient.set_peer_name(new_text).finished
-	if result.has_error():
-		push_error(result.error)
