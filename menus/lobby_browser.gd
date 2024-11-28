c0d1112ff4c8c8574bddb350258790cbf9b65909
Dragos Daian <daian.dragos@yahoo.com>
2024-11-28 09:46:06 +0100

    update code 123

--- menus/lobby_browser.gd
+++
@@ -1,6 +0,0 @@
-extends ColorRect
-
-var main_menu_scene : PackedScene = load("res://menus/main_menu.tscn")
-
-func _on_button_main_menu_pressed() -> void:
-	get_tree().change_scene_to_packed(main_menu_scene)
