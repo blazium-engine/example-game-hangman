d878d4589683f7a39a035ab70f9a3b84fdf72195
Dragos Daian <daian.dragos@yahoo.com>
2024-11-29 23:29:30 +0100

    update demo

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -3,15 +3,17 @@ extends Control
 var lobby_browser_scene : PackedScene = load("res://menus/lobby_browser/lobby_browser.tscn")
 var lobby_creator_scene : PackedScene = load("res://menus/lobby_creator.tscn")
 var reconnects = 0
+@onready var peer_name : LineEdit = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/LineEdit
 @onready var lobby_connection_label : Label = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/Label
 
 func _ready() -> void:
+	peer_name.text = GlobalLobbyClient.peer.peer_name
 	GlobalLobbyClient.append_log.connect(_on_logs)
 	GlobalLobbyClient.disconnected_from_lobby.connect(_on_disconnect)
 	connect_to_lobby()
 
 func connect_to_lobby():
-	GlobalLobbyClient.server_url = "ws://localhost:8080/connect"
+	#GlobalLobbyClient.server_url = "ws://localhost:8080/connect"
 	var connected = GlobalLobbyClient.connect_to_lobby("Hangman")
 	if connected:
 		lobby_connection_label.text = "Lobby Service: Connected"
