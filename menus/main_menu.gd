3a448d26d1dfb73c7f071ade92954702f4723a8f
Dragos Daian <daian.dragos@yahoo.com>
2024-11-30 22:55:23 +0100

    Remove localhost reference (#5)
    * Update main_menu.gd
    
    * remove local host

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -3,17 +3,20 @@ extends Control
 var lobby_browser_scene : PackedScene = load("res://menus/lobby_browser/lobby_browser.tscn")
 var lobby_creator_scene : PackedScene = load("res://menus/lobby_creator.tscn")
 var reconnects = 0
-@onready var peer_name : LineEdit = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/LineEdit
-@onready var lobby_connection_label : Label = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/Label
-@onready var logs : Label = $HBoxContainer2/ColorRect/VBoxContainer/VBoxContainer/VBoxContainer/Logs
 
+@export var peer_name_line_edit : LineEdit
+@export var lobby_connection_label : Label
+@export var logs : Label
+@export var menu: VBoxContainer
+@export var set_name_menu: VBoxContainer
 func _ready() -> void:
-	peer_name.text = GlobalLobbyClient.peer.peer_name
 	GlobalLobbyClient.disconnected_from_lobby.connect(_on_disconnect)
 	connect_to_lobby()
+	peer_name_line_edit.text = GlobalLobbyClient.peer.peer_name
+	menu.visible = GlobalLobbyClient.peer.peer_name != ""
 
 func connect_to_lobby():
-	GlobalLobbyClient.server_url = "ws://localhost:8080/connect"
+	#GlobalLobbyClient.server_url = "ws://localhost:8080/connect"
 	var connected = GlobalLobbyClient.connect_to_lobby("hangman")
 	if connected:
 		lobby_connection_label.text = "Lobby Service: Connected"
@@ -35,9 +38,11 @@ func _on_button_lobby_pressed() -> void:
 	get_tree().change_scene_to_packed(lobby_creator_scene)
 
 
-func _on_line_edit_text_submitted(new_text: String) -> void:
-	var result :LobbyResult= await GlobalLobbyClient.set_peer_name(new_text).finished
+func _on_set_name_pressed() -> void:
+	var result :LobbyResult= await GlobalLobbyClient.set_peer_name(peer_name_line_edit.text).finished
 	if result.has_error():
 		logs.text = result.error
 	else:
 		logs.text = "Success"
+		menu.visible = true
+		set_name_menu.visible = false
